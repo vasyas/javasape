d6f48d7d292915447400a92c95299c7ec8f3318f
vasyl.stashuk <vasyl.stashuk@7890d57c-c7a8-11dd-89f9-7f0a91924d23>
2008-12-11 19:56:34 +0000

    misc fixes

--- src/java/javasape/Sape.java
+++ src/java/javasape/Sape.java
@@ -2,8 +2,6 @@ package javasape;
 
 import javax.servlet.http.Cookie;
 
-import org.apache.log4j.BasicConfigurator;
-
 public class Sape {
     private final String sapeUser;
     
@@ -21,15 +19,5 @@ public class Sape {
     
     public SapePageLinks getPageLinks(String requestUri, Cookie[] cookies) {
         return new SapePageLinks(sapePageLinkConnection, sapeUser, requestUri, cookies, debug);
-    }
-    
-    public static void main(String[] args) {
-        BasicConfigurator.configure();
-        
-        Sape sape = new Sape("b4e26b5106ddf32e5e9bc07626d1c029", "www.investportfel.com", 1000, 10);
-    
-        SapePageLinks pageLinks = sape.getPageLinks("/funds/", null);
-        
-        System.out.println(pageLinks.render());
     }
 }
